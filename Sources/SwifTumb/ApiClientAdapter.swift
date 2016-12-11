ef8de19ddf6a1699349872f49b7d219c7e3bd857
Ushio Shugo <ushio.s@gmail.com>
2016-12-11 16:10:13 +0900

    Using env in convinence

--- Sources/SwifTumb/ApiClientAdapter.swift
+++ Sources/SwifTumb/ApiClientAdapter.swift
@@ -7,12 +7,16 @@
 //
 
 import Foundation
+import OAuthSwift
 
 protocol ApiClientAdapter {
     func get(path: String) -> String
 }
 
 class OAuthSwiftAdapter {
+    private static let EnvKeyConsumerKey: String = "SWIFTUMB_CONSUMER_KEY"
+    private static let EnvKeyConsumerSecret: String = "SWIFTUMB_CONSUMER_SECRET"
+    
     private let Host: String = "api.tumblr.com"
     private let Version: String = "v2"
     
@@ -20,20 +24,40 @@ class OAuthSwiftAdapter {
     private let AuthorizeUrl: String = "https://www.tumblr.com/oauth/authorize"
     private let AccessTokenUrl: String = "https://www.tumblr.com/oauth/access_token"
     
-    
     private var consumerKey: String
     private var consumerSecret: String
+    private var oauthswift: OAuthSwift
+    private var handle: OAuthSwiftRequestHandle?
     
     init(consumerKey: String, consumerSecret: String) {
         self.consumerKey = consumerKey
         self.consumerSecret = consumerSecret
+        
+        self.oauthswift = OAuth1Swift(
+            consumerKey: self.consumerKey,
+            consumerSecret: self.consumerSecret,
+            requestTokenUrl: self.RequestTokenUrl,
+            authorizeUrl: self.AuthorizeUrl,
+            accessTokenUrl: self.AccessTokenUrl
+        )
+        
+        self.handle = self.oauthswift
     }
     
     convenience init() {
-        self.init(consumerKey: "", consumerSecret: "")
+        let key = getEnvironmentVar(name: OAuthSwiftAdapter.EnvKeyConsumerKey) ?? ""
+        let secret = getEnvironmentVar(name: OAuthSwiftAdapter.EnvKeyConsumerSecret) ?? ""
+        
+        self.init(consumerKey: key, consumerSecret: secret)
     }
     
     public func baseUrl() -> String {
         return self.Host + "/" + self.Version
     }
+}
+
+
+func getEnvironmentVar(name: String) -> String? {
+    guard let rawValue = getenv(name) else { return nil }
+    return String(utf8String: rawValue)
 }
